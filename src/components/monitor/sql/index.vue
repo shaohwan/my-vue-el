83b5fb0bfbfd61aac7505af7e9ee8efbd2071734
Daniel Wang <daniel.wang@carel.com>
2025-06-16 16:51:09 +0800

    add sql monitor

---
+++ src/components/monitor/sql/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-button type="primary" @click="goToDruid">查看SQL监控</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    goToDruid() {
+      window.location.href = `${import.meta.env.VITE_API_BASE_URL}/druid/`
+    },
+  },
+}
+</script>
+
+<style scoped>
+/* 自定义样式 */
+</style>
